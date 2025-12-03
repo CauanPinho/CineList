4fd2f2fcd1149cad145806b97e33a92cccd47961
Cauan Pinho <Jobscauanpinho@gmail.com>
2025-12-03 11:00:51 -0400

    Base do projeto

--- src/Application.java
+++ src/Application.java
@@ -1,15 +1,82 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import entities.Movie;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Application{
+    public static void main(String[] args){
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        Scanner sc=new Scanner(System.in);
+        ArrayList<Movie> movies=new ArrayList<>();
+        boolean running=true;
+
+        while(running){
+            System.out.println("===MENU===\n" +
+                    "1 - CADASTRAR FILME\n" +
+                    "2 - LISTAR FILMES\n " +
+                    "3 - PROCURAR FILME\n" +
+                    "4 - ATUALIZAR CADASTRO DO FILME\n" +
+                    "5- REMOVER FILME\n" +
+                    "6 - SAIR\n");
+            int option;
+            option=sc.nextInt();
+            sc.nextLine();
+
+
+            switch(option){
+                case 1:
+                    System.out.println("Cadastre o id do filme\n");
+                    int id=sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.println("Cadastre o titulo do filme\n");
+                    String titulo=sc.nextLine();
+
+                    System.out.println("Cadastre o genero do filme\n ");
+                    String genero=sc.nextLine();
+
+                    System.out.println("Cadatre o ano deste filme \n");
+                    int ano=sc.nextInt();
+                    sc.nextLine();
+
+                    Movie m = new Movie(id,titulo,genero,ano);
+                    movies.add(m);
+
+                    System.out.println("Item cadastrado com sucesso...\n");
+                    break;
+
+                case 2:
+                    System.out.println("===LISTA DE FILMES CADASTRADOS\n");
+
+                    if(movies.isEmpty()){
+                        System.out.println("N existe filme cadastrado ainda, clique na opção 1 e cadastre-os\n");
+                    }else{
+                        for (int i =0;i<movies.size();i++){
+                            Movie item= movies.get(i);
+                            System.out.println("Posição" + (i+1) +
+                                    "| ID:\n " + item.getId() +
+                                    "| TITULO: \n" + item.getTitulo() +
+                                    "| GENERO:\n " + item.getGenero() +
+                                    "| ANO: \n" + item.getAno());
+                        }
+                    }
+                    break;
+                case 3:
+                    System.out.println("===PROCURAR LIVRO===\n");
+                    System.out.println("Digite a nome do livro");
+
+                    String termo=sc.nextLine().tolowerCase();
+
+                    boolean encontrado = false;
+
+                    for(Movie item : movies){
+                        if(item.getTitulo.toLowerCase().contains(termo)){
+                            System.out.println("| ID " + item.get(i));
+                        }
+                    }
+
+
+            }
         }
+
     }
 }
--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -1,5 +1,55 @@
 package entities;
 
-public class Movie{
-    private
+public class Movie {
+    private int id;
+    private String titulo;
+    private String genero;
+    private int ano;
+
+
+    // CONTRUTOR DO MEU todo
+    public Movie(int id, String titulo, String genero, int ano) {
+        this.id = id;
+        this.titulo = titulo;
+        this.genero = genero;
+        this.ano = ano;
+
+    }
+
+    //GETTERS
+    public int getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+
+    // SETTERS
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+
 }
